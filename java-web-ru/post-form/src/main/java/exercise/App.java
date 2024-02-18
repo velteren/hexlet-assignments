18bbb168933ec169b3104060e9492e3ddea3fdf2
@hexlet/cli <support@hexlet.io>
2024-02-18 11:27:09 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,59 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Collections;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+
+        // BEGIN
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+
+        app.post("/users", ctx -> {
+            var name = ctx.formParam("firstname").trim();
+            var lastname = ctx.formParam("lastname").trim();
+            var email = ctx.formParam("email").trim().toLowerCase();
+            var password = ctx.formParam("password");
+            var passwordConfirm = ctx.formParam("passwordConfirmation");
+            if (password != passwordConfirm) {
+                ctx.result("wrong password");
+                ctx.redirect("/users/build");
+            }
+            var encrpyptedPass = Security.encrypt(password);
+            var newUser = new User(name, lastname, email, encrpyptedPass);
+            UserRepository.save(newUser);
+            ctx.redirect("/users");
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
